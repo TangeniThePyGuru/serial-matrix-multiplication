e2981a5abf0dd4ece3888e74841417553f0d0a8f
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-04-24 03:59:02 -0400

    error fix

--- serialMatrixMultiplication.cu
+++ serialMatrixMultiplication.cu
@@ -20,7 +20,7 @@ inline cudaError_t checkCuda(cudaError_t result)
 //   }
 // }
 
-void multMatricesInto(float *result, float *a, float *b, int N)
+void multMatricesInto(int N, float result[][N], float a[][N], float b[][N])
 {
   for (unsigned int i = 0; i < N; i++) {
 		for (unsigned int j = 0; j < N; j++) {
@@ -118,7 +118,7 @@ int main(int argc, char **argv)
   printf("Performing Mutrix Multiplication..."); fflush(stdout);
   startTime(&timer);
 
-  multMatricesInto(&c, &a, &b, numCRows);
+  multMatricesInto(numCRows, c, a, b);
 
 
   // printf("Verifying Mutrix Multiplication..."); fflush(stdout);
